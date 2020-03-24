48cf564c8bf306b454e3b651c33688f6e3f000cd
syncline0605 <hanjing19990713@163.com>
2020-03-24 09:36:22 +0800

    <feat>: 补充定义了三种异常类型，现共七种
    原异常：
    坐标范围超限！
    参数个数有误！
    命令行参数是其他字符！
    交点个数为无穷个！
    补充异常：
    非法输入格式！
    输入的几何对象不足！
    文件尾输入了无关的信息或过多的几何对象！

--- src/Exception.h
+++ src/Exception.h
@@ -31,3 +31,27 @@ struct infException : public exception
 		return "交点个数为无穷个！";
 	}
 };
+
+struct illegalInputPattern : public exception
+{
+	const char* what() const throw ()
+	{
+		return "非法输入格式！";
+	}
+};
+
+struct notEnoughInputElement : public exception
+{
+	const char* what() const throw ()
+	{
+		return "输入的几何对象不足！";
+	}
+};
+
+struct TooManyInputElements : public exception
+{
+	const char* what() const throw ()
+	{
+		return "文件尾输入了无关的信息或过多的几何对象！";
+	}
+};
